6abf4c0636a68b0a83f6cc6a27833907d87d5f78
xerocuil <mail@xerocuil.com>
2022-12-29 17:02:48 -0500

    2022.1229.1702

--- pywin.sh
+++ pywin.sh
@@ -1,58 +1,80 @@
 #! /bin/bash
+
+# Pywin
+
+## Manage python environments for wine.
+
+## Sofware sources
+### -[NSIS 3.08](https://versaweb.dl.sourceforge.net/project/nsis/NSIS%203/3.08/nsis-3.08-setup.exe)
+### -[Python 3.11.1](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe)
+
+
+## Global variables
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROFILE_DIR=$HOME/.pywin
-CONFIG=$SCRIPT_DIR/config.bat
-CONFIGTMP=$SCRIPT_DIR/config.txt
 TMPDIR=$PROFILE_DIR/tmp
-
 VENV="$PROFILE_DIR/venv"
 PFX="$PROFILE_DIR/pfx"
+CONFIG=$SCRIPT_DIR/config.bat
+CONFIGTMP=$SCRIPT_DIR/config.txt
 CWD="$(pwd)"
 CMD="$1"
 
+
+## Wine settings
 export WINEDEBUG=-all
 export WINEDLLOVERRIDES=winemenubuilder.exe=d
 export WINEPATH="$SCRIPT_DIR"
 export WINEPREFIX="$PFX"
 
+
+## Functions
+
+### Winecfg
 cfg(){
   winecfg
 }
 
+### Open Python prefix in bash terminal
 cmd(){
   wine cmd
 }
 
+### Open Python prefix in wine console
 console(){
   wineconsole
 }
 
-## Create config file
+### Create config file
 create_config(){
   >$CONFIG
   >$CONFIGTMP
 
-  ### Set profile directory
+  #### Set profile directory
   cd "$PROFILE_DIR"
   wineconsole "$SCRIPT_DIR/wcf.bat" "-d"
 
-  ### Set script directory
+  #### Set script directory
   cd "$SCRIPT_DIR"
   wineconsole "$SCRIPT_DIR/wcf.bat" "-d"
 
-  ### Set python version
+  #### Set python version
   wineconsole "$SCRIPT_DIR/wcf.bat" "-v"
 
+  #### Parse temp config file
   IFS=$'\n'
   CONFIGOPTS=($(cat $CONFIGTMP))
-  APP_DIR=$(echo ${CONFIGOPTS[1]} | sed 's/ *$//g')
-  
+
+  #### Create config.bat file
   echo -e "set PROFILE_DIR=${CONFIGOPTS[0]}
-set APP_DIR=$APP_DIR
+set APP_DIR=${CONFIGOPTS[1]}
 set VERSION=${CONFIGOPTS[2]}" >>$CONFIG
+
+  #### Clear temp config file/return to CWD
   rm $CONFIGTMP
   cd $CWD
 }
+
 
 ## Create environment if missing
 if [[ ! -d $PFX ]]; then
@@ -80,18 +102,16 @@ if [[ ! -d $PFX ]]; then
   ### Create config file
   create_config
 else
+  ### Remove temp directory if environment found
   if [[ -d $TMPDIR ]]; then
     rm -rf $TMPDIR
   fi
 fi
 
+
+## Run `wine cmd` if no command given
 if [[ -z $CMD ]]; then
   wine cmd
 else
   $CMD
 fi
-
-
-## Sofware sources
-### -[NSIS 3.08](https://versaweb.dl.sourceforge.net/project/nsis/NSIS%203/3.08/nsis-3.08-setup.exe)
-### -[Python 3.11.1](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe)
