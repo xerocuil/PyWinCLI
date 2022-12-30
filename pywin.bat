5c29431a42328ceffcdac25397c5effea3e154cc
xerocuil <tbrodhead@mm.st>
2022-12-30 00:20:53 -0500

    +installer

--- pywin.bat
+++ pywin.bat
@@ -1,46 +1,59 @@
 @echo off
 
+rem # PyWin
+
+rem ## Global variables
 set SCRIPT_DIR=%~dp0
-call %SCRIPT_DIR%\config.bat
+set PROFILE_DIR=Z:\home\%USER%\.pywin
 set VDIR=%PROFILE_DIR%\venv
 set VENV=%VDIR%\default
 set OPT1=%1
 set OPT2=%2
 
+rem ## Options
+
+rem ### Default
 if "%OPT1%"=="" (
   echo ## Default virtual environment
   goto initalize
 )
 
+rem ### List
 if "%OPT1%"=="-l" (
   echo ## PyWin Environments
   dir /A:D /B %VDIR%
   exit /b
 )
 
+rem ### Load
 if "%OPT1%"=="-p" (
   set VENV=%VDIR%\%OPT2%
   echo ## %OPT2% virtual environment
   goto initalize
 ) 
 
+rem ### Remove
 if "%OPT1%"=="-r" (
   set VENV=%VDIR%\%OPT2%
   goto remove
 )
 
+rem ## Functions
+
+rem ### Initialize virtual environment
 :initalize
 if exist %VENV% (
   echo ### Loading virtual environment
   goto activate
 ) else (
-  echo ### Creating Virtual Environment
+  echo ### Creating virtual Environment
   python -m venv %VENV%
   call %VENV%\Scripts\activate
   python -m pip install --upgrade pip
   goto activate
 )
 
+rem ### Remove selected virtual environment
 :remove
 if exist %VENV% (
   echo ## Deleting %OPT2% virtual environment
@@ -51,5 +64,7 @@ if exist %VENV% (
   exit /b
 )
 
+rem ### Activate environment
 :activate
   call %VENV%\Scripts\activate
+  
--- pywin.sh
+++ pywin.sh
@@ -1,35 +1,27 @@
 #! /bin/bash
 
-# Pywin
+# PyWin
 
 ## Manage python environments for wine.
 
-## Sofware sources
-### -[NSIS 3.08](https://versaweb.dl.sourceforge.net/project/nsis/NSIS%203/3.08/nsis-3.08-setup.exe)
-### -[Python 3.11.1](https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe)
-
-
 ## Global variables
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROFILE_DIR=$HOME/.pywin
+SCRIPTS=$PROFILE_DIR/scripts
 TMPDIR=$PROFILE_DIR/tmp
 VENV="$PROFILE_DIR/venv"
 PFX="$PROFILE_DIR/pfx"
-CONFIG=$SCRIPT_DIR/config.bat
-CONFIGTMP=$SCRIPT_DIR/config.txt
+CONFIG=$SCRIPTS/config.bat
+CONFIGTMP=$SCRIPTS/config.txt
 CWD="$(pwd)"
 CMD="$1"
-
 
 ## Wine settings
 export WINEDEBUG=-all
 export WINEDLLOVERRIDES=winemenubuilder.exe=d
-export WINEPATH="$SCRIPT_DIR"
+export WINEPATH="$SCRIPTS"
 export WINEPREFIX="$PFX"
 
-
 ## Functions
-
 ### Winecfg
 cfg(){
   winecfg
@@ -45,73 +37,25 @@ console(){
   wineconsole
 }
 
-### Create config file
-create_config(){
-  >$CONFIG
-  >$CONFIGTMP
+help(){
+  echo -e "
+  pwcli.sh <command>
 
-  #### Set profile directory
-  cd "$PROFILE_DIR"
-  wineconsole "$SCRIPT_DIR/wcf.bat" "-d"
-
-  #### Set script directory
-  cd "$SCRIPT_DIR"
-  wineconsole "$SCRIPT_DIR/wcf.bat" "-d"
-
-  #### Set python version
-  wineconsole "$SCRIPT_DIR/wcf.bat" "-v"
-
-  #### Parse temp config file
-  IFS=$'\n'
-  CONFIGOPTS=($(cat $CONFIGTMP))
-
-  #### Create config.bat file
-  echo -e "set PROFILE_DIR=${CONFIGOPTS[0]}
-set APP_DIR=${CONFIGOPTS[1]}
-set VERSION=${CONFIGOPTS[2]}" >>$CONFIG
-
-  #### Clear temp config file/return to CWD
-  rm $CONFIGTMP
-  cd $CWD
+  Command | Description
+  ------- | -----------
+  cmd     | Open PyWin in bash terminal
+  console | Open PyWin in wine console 
+  "
 }
-
 
 ## Create environment if missing
 if [[ ! -d $PFX ]]; then
-  ## Win 10/64-bit wine prefix
-  winetricks -q mimeassoc=off win10
-  mkdir -p $TMPDIR
-  cd $TMPDIR
-
-  ### Install Python 3.11.1 (64-Bit)
-  wget https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe
-  if [[ -f python-3.11.1-amd64.exe ]]; then
-    wine python-3.11.1-amd64.exe
-  else
-    echo "Please download python-3.11.1-amd64.exe to this directory."
-  fi
-
-  ### Install NSIS 3.08
-  wget https://versaweb.dl.sourceforge.net/project/nsis/NSIS%203/3.08/nsis-3.08-setup.exe
-  if [[ -f nsis-3.08-setup.exe ]]; then
-    wine nsis-3.08-setup.exe
-  else
-    echo "Please download nsis-3.08-setup.exe to this directory."
-  fi
-
-  ### Create config file
-  create_config
-else
-  ### Remove temp directory if environment found
-  if [[ -d $TMPDIR ]]; then
-    rm -rf $TMPDIR
-  fi
+  echo -e "Please run install.sh script."
 fi
-
 
 ## Run `wine cmd` if no command given
 if [[ -z $CMD ]]; then
-  wine cmd
+  help
 else
   $CMD
 fi
